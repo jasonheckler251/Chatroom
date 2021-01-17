5a0fd5bb97e90d6c11187d93adeaae983361bf18
Jason Heckler <jasonheckler251@gmail.com>
2021-01-17 12:25:45 -0500

    Updating Chatroom_Server.py

--- Chatroom_Server.py
+++ Chatroom_Server.py
@@ -10,16 +10,16 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host,port))
 
 def handle_clients(conn, address):
-    name = conn.rec(1024).decode()
+    name = conn.recv(1024).decode()
     welcome = "Welcome " + name + " You can type #quit if you want to leave the chatroom."
-    conn.recv(bytes(welcome, "utf8"))
+    conn.send(bytes(welcome, "utf8"))
     msg = name + " has recently joined the chat room!"
     broadcast(bytes(msg, "utf8"))
     clients[conn] = name
 
     while True:
         msg = conn.recv(1024)
-        if msg != byte("#quit", "utf8"):
+        if msg != bytes("#quit", "utf8"):
             broadcast(msg, name + ":")
         else:
             conn.send(bytes("#quit", "utf8"))
